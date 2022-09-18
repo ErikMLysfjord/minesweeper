3ba0d332f5b5dd1349b30ab7393ed8043478543d
oarorvik <oarorvik@stud.ntnu.no>
2022-09-18 14:16:40 +0200

    Registrering av klikket rute.

--- minesweeper/src/main/java/minesweeper/MinesweeperController.java
+++ minesweeper/src/main/java/minesweeper/MinesweeperController.java
@@ -1,16 +1,22 @@
 package minesweeper;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 
 public class MinesweeperController {
 
     public MinesweeperController() {
-        
+
     }
 
+    @FXML
+    void handleClickedSquare(ActionEvent actionEvent) {
+        System.out.println("click");
+    }
 
     @FXML
     void initialize() {
+
     }
 
 }
