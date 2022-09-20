816a60eb91b1914e1119a3f5a710c344ed8a48a2
oarorvik <oarorvik@stud.ntnu.no>
2022-09-20 16:41:39 +0200

    Removed imports

--- minesweeper/src/test/java/minesweeper/MinefieldTest.java
+++ minesweeper/src/test/java/minesweeper/MinefieldTest.java
@@ -1,11 +1,8 @@
 package minesweeper;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
 
 public class MinefieldTest {
 
