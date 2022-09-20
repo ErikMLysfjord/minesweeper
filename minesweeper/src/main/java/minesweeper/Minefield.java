45131026aabc9ee38e6d1163cabacc919682d166
oarorvik <oarorvik@stud.ntnu.no>
2022-09-20 16:19:45 +0200

    setSquare and error handling

--- minesweeper/src/main/java/minesweeper/Minefield.java
+++ minesweeper/src/main/java/minesweeper/Minefield.java
@@ -5,6 +5,8 @@ import java.util.List;
 
 public class Minefield {
     private final List<List<Square>> minefield;    
+    private final int width;
+    private final int height;
 
     public Minefield(int width, int height) {
         if (width < 1 || height < 1) {
@@ -15,6 +17,9 @@ public class Minefield {
         for (int i = 0; i < height ; i++) {
             minefield.add(new ArrayList<>());
         }
+
+        this.width = width;
+        this.height = height;
     }
 
     //TODO
@@ -22,13 +27,19 @@ public class Minefield {
         return null;
     }
 
-    //TODO
     public void setSquare(Square square, int x, int y) {
-        
+        if (isOutOfBounds(x, y)) {
+            throw new IllegalStateException("Coordinates are out of bounds");
+        }
+        minefield.get(y).set(x, square);
     }
 
     private boolean isOutOfBounds(int x, int y) {
-        return false; //TODO
+        return
+            x < 0 ||
+            x >= width ||
+            y < 0 ||
+            y >= height;
     }
 
 }
