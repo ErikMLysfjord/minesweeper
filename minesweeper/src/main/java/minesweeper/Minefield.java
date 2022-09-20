5b7511b6dc876c7682502aef9176b325347ad5f1
oarorvik <oarorvik@stud.ntnu.no>
2022-09-20 16:08:38 +0200

    Minefield constructor

--- minesweeper/src/main/java/minesweeper/Minefield.java
+++ minesweeper/src/main/java/minesweeper/Minefield.java
@@ -1,9 +1,20 @@
 package minesweeper;
 
-public class Minefield {
-    
-    public Minefield(int width, int height) {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Minefield {
+    private final List<List<Square>> minefield;    
+
+    public Minefield(int width, int height) {
+        if (width < 1 || height < 1) {
+            throw new IllegalStateException("Must have dimentions larger than 0.");
+        }
+
+        minefield = new ArrayList<>();
+        for (int i = 0; i < height ; i++) {
+            minefield.add(new ArrayList<>());
+        }
     }
 
     //TODO
@@ -14,6 +25,10 @@ public class Minefield {
     //TODO
     public void setSquare(Square square, int x, int y) {
         
+    }
+
+    private boolean isOutOfBounds(int x, int y) {
+        return false; //TODO
     }
 
 }
