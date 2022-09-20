ed67f89227b63cd2df9c238c1d5c27affeef5fde
oarorvik <oarorvik@stud.ntnu.no>
2022-09-20 15:53:17 +0200

    Basic Minefield tests

--- minesweeper/src/main/java/minesweeper/Minefield.java
+++ minesweeper/src/main/java/minesweeper/Minefield.java
@@ -2,4 +2,18 @@ package minesweeper;
 
 public class Minefield {
     
+    public Minefield(int width, int height) {
+
+    }
+
+    //TODO
+    public Square getSquare(int x, int y) {
+        return null;
+    }
+
+    //TODO
+    public void setSquare(Square square, int x, int y) {
+        
+    }
+
 }
