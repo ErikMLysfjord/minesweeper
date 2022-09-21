99a46b44194dd4f27627559dbeaf891a377e8f1a
Erik Menkin Lysfjord <erikmly@smint.no>
2022-09-21 14:29:02 +0200

    added dependency for json and updated module-info

--- minesweeper/src/main/java/module-info.java
+++ minesweeper/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module minesweeper {
     requires javafx.controls;
     requires javafx.fxml;
+    requires json.simple;
 
-    opens minesweeper to javafx.graphics, javafx.fxml;
+    opens minesweeper to javafx.graphics, javafx.fxml, json.simple;
 }
