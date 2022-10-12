881f9f18806473281615a3a34d572f280c913aa0
vebjørn blom-hagen <vebjorbl@stud.ntnu.no>
2022-10-12 12:38:29 +0200

    test: make filetreatertest
    Make folder and files for testing datahandling.

--- minesweeper/core/src/main/java/minesweeper/json/FileTreater.java
+++ minesweeper/core/src/main/java/minesweeper/json/FileTreater.java
@@ -5,13 +5,13 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import minesweeper.core.HighscoreEntry;
 
 public class FileTreater {
-
+    private static String path = "../core/src/main/resources/minesweeper/json/data.json";
     /**
      * Saves serialized score object to data.json.
      * @param score the score to be saved
      */
     public static void saveScore(HighscoreEntry score) {
-        File data = new File("../core/src/main/resources/minesweeper/json/data.json");
+        File data = new File(path);
         ObjectMapper mapper = new ObjectMapper();
         try {
             mapper.writeValue(data, score);
---
+++ minesweeper/core/src/test/java/minesweeper/json/FileTreaterTest.java
@@ -0,0 +1,20 @@
+package minesweeper.json;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class FileTreaterTest {
+    
+    @BeforeEach
+    public void setUp() {
+
+    }
+
+    @Test
+    public void testSaveScore(){
+        
+    }
+
+
+}
