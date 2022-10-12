b0a3cb87a830d7722f5cb21c6f2ff76ecf90dca5
vebjørn blom-hagen <vebjorbl@stud.ntnu.no>
2022-10-12 16:13:38 +0200

    test: make test for retrieving and storing data
    Testing retrive data.
    Modified FileTreater so there is a function for retrieving data.
    Need to change the format for storing data.

--- minesweeper/core/src/test/java/minesweeper/json/FileTreaterTest.java
+++ minesweeper/core/src/test/java/minesweeper/json/FileTreaterTest.java
@@ -11,22 +11,25 @@ import minesweeper.core.HighscoreEntry;
 
 public class FileTreaterTest {
 
-    private List<HighscoreEntry> entries;
+    private List<HighscoreEntry> entries = new ArrayList<HighscoreEntry>();
     /* private String[] names;
     private Integer[] scores; */
     private String rPath;
-    private Random rand;
+    private Random rand = new Random();
     private Integer scoreRoof;
+    private List<Integer> scores;
     
     @BeforeEach
     public void setUp() {
         scoreRoof = 1000;
         rPath = "../core/src/main/resources/minesweeper/json/data.json";
         String[] names = {"Ole", "Dole", "Toern", "Erik", "Vebjørn","David", "Marie", "Sofie", "Mathilde", "Andreas"};
-        
+        Integer i;
         for (String name : names) {
-            HighscoreEntry entry = new HighscoreEntry(name, rand.nextInt(scoreRoof));
+            i = rand.nextInt(scoreRoof);
+            HighscoreEntry entry = new HighscoreEntry(name, i);
             entries.add(entry);
+            scores.add(i);
         }
         
     }
@@ -35,11 +38,7 @@ public class FileTreaterTest {
     public void testSaveScore(){
         Assertions.assertEquals(rPath, FileTreater.getPath());
 
-        for (HighscoreEntry entry: entries) {
-            FileTreater.saveScore(entry);
-        }
-
-        
+        Assertions.assertEquals(FileTreater.getScore(entries.get(0).getName()), scores.get(0));
     }
 
 
