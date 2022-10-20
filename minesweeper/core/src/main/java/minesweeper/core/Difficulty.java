9f6f3b33f12d777eb1ecf0356346d2816b8da8ee
Andreas Li <andrea10@stud.ntnu.no>
2022-10-20 15:43:32 +0200

    feat: finish Difficulty class and test

--- minesweeper/core/src/main/java/minesweeper/core/Difficulty.java
+++ minesweeper/core/src/main/java/minesweeper/core/Difficulty.java
@@ -5,38 +5,86 @@ public class Difficulty {
     private int height;
     private int minesAmount;
 
+    /**
+     * A helping constructor for difficulty
+     * @param width the width for difficulty
+     * @param height the height for difficulty
+     * @param minesAmount the amount of mines for difficulty
+     */
     public Difficulty(int width, int height, int minesAmount) {
         this.width = width;
         this.height = height;
         this.minesAmount = minesAmount;
     }
 
-    public void easy() {
+    /**
+     * A constructor for difficulty
+     * @param difficulty easy, medium or hard difficulty as a string
+     * @throws IllegalArgumentException if difficulty is not easy, medium or hard
+
+     */
+    public Difficulty(String difficulty) {
+        if (difficulty == "easy") {
+            setEasy();
+        }
+        else if (difficulty == "medium") {
+            setMedium();
+        }
+        else if (difficulty == "hard") {
+            setHard();
+        }
+        else {
+            throw new IllegalArgumentException("Difficulty must be easy, medium or hard");
+        }
+    }
+
+    /**
+     * Sets difficulty's parameters to static easy values
+     */
+    private void setEasy() {
         this.width = 9;
         this.height = 9;
         this.minesAmount = 10;
     }
 
-    public void medium() {
+    /**
+     * Sets difficulty's parameters to static medium values
+     */
+    private void setMedium() {
         this.width = 16;
         this.height = 16;
         this.minesAmount = 40;
     }
 
-    public void hard() {
+    /**
+     * Sets difficulty's parameters to static hard values
+     */
+    private void setHard() {
         this.width = 30;
         this.height = 16;
         this.minesAmount = 99;
     }
 
+    /**
+     * Gets the width of the difficulty
+     * @return the width of the difficulty
+     */
     public int getWidth() {
         return width;
     }
 
+    /**
+     * Gets the height of the difficulty
+     * @return the height of the difficulty
+     */
     public int getHeight() {
         return height;
     }
 
+    /**
+     * Gets the amount of mines of the difficulty
+     * @return the amount of mines of the difficulty
+     */
     public int getMinesAmount() {
         return minesAmount;
     }
--- minesweeper/core/src/test/java/minesweeper/core/DifficultyTest.java
+++ minesweeper/core/src/test/java/minesweeper/core/DifficultyTest.java
@@ -1,42 +1,38 @@
 package minesweeper.core;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class DifficultyTest {
     private Difficulty difficulty;
 
-    @BeforeEach
-    public void setup() {
-        difficulty = new Difficulty(0, 0, 0);
-    }
-
     @Test
-    public void testEasy() {
-        Assertions.assertEquals(0, difficulty.getWidth());
-        difficulty.easy();
+    public void testSetEasy() {
+        difficulty = new Difficulty("easy");
         Assertions.assertEquals(9, difficulty.getWidth());
         Assertions.assertEquals(9, difficulty.getHeight());
         Assertions.assertEquals(10, difficulty.getMinesAmount());
     }
 
     @Test
-    public void testMedium() {
-        Assertions.assertEquals(0, difficulty.getHeight());
-        difficulty.medium();
+    public void testSetMedium() {
+        difficulty = new Difficulty("medium");
         Assertions.assertEquals(16, difficulty.getWidth());
         Assertions.assertEquals(16, difficulty.getHeight());
         Assertions.assertEquals(40, difficulty.getMinesAmount());
     }
 
     @Test
-    public void testHard() {
-        Assertions.assertEquals(0, difficulty.getMinesAmount());
-        difficulty.hard();
+    public void testSetHard() {
+        difficulty = new Difficulty("hard");
         Assertions.assertEquals(30, difficulty.getWidth());
         Assertions.assertEquals(16, difficulty.getHeight());
         Assertions.assertEquals(99, difficulty.getMinesAmount());
+    }
+
+    @Test
+    public void testIllegalDifficulty() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> new Difficulty("expert"));
     }
 
 }
