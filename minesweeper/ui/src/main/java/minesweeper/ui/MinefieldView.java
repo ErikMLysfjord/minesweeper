2e77d27f7d982a6975714bd081122c5c827f3140
Vebjørn Falk Blom-Hagen <vebjorbl@stud.ntnu.no>
2022-10-20 14:00:08 +0000

    style: fix trailing space

--- minesweeper/ui/src/main/java/minesweeper/ui/MinefieldView.java
+++ minesweeper/ui/src/main/java/minesweeper/ui/MinefieldView.java
@@ -18,7 +18,7 @@ public class MinefieldView {
     private List<List<ImageView>> squareImages;
     private List<List<Button>> squareButtons;
 
-    private final Image bombImage = 
+    private final Image bombImage =
         new Image(getClass().getResourceAsStream("bomb.png"));
 
     private final Image flagImage =
