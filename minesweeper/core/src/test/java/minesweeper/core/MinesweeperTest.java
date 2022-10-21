e5e9a03bfe3a678505bf54a9a698c9222203a37c
oarorvik <oarorvik@stud.ntnu.no>
2022-10-21 14:03:12 +0200

    test: test safe squares on first open
    Tests that you don't lose if you open the first square, or try
    to open its adjacent squares. These squares should be safe.

--- minesweeper/core/src/test/java/minesweeper/core/MinesweeperTest.java
+++ minesweeper/core/src/test/java/minesweeper/core/MinesweeperTest.java
@@ -1,17 +1,21 @@
 package minesweeper.core;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class MinesweeperTest {
     private Minesweeper minesweeper;
+    private boolean isWon;
+    private boolean isLost;
 
     @BeforeEach
     public void setupMinesweeper() {
-        minesweeper = new Minesweeper(3, 3);
+        minesweeper = new Minesweeper(4, 4);
+        isWon = false;
+        isLost = false;
     }
 
-    private boolean isLost;
     @Test
     public void testLosing() {
         minesweeper.addOnLoss(() -> {
@@ -20,11 +24,29 @@ public class MinesweeperTest {
         //Needs mines in minefield
     }
 
-    private boolean isWon;
+    @Test
     public void testWinning() {
         minesweeper.addOnWin(() -> {
             isWon = true;
         });
         //Needs mines in minefield
+    }
+
+    @Test
+    public void testStartingSafeSquares() {
+        minesweeper.addOnLoss(() -> {
+            isLost = true;
+        });
+
+        Integer[][] coords = {
+            {1,1}, {0,0}, {0,1},
+            {0,2}, {1,0}, {1,2},
+            {2,0}, {2,1}, {2,2}
+        };
+        
+        for (Integer[] coord : coords) {
+            minesweeper.openSquare(coord[0], coord[1]);
+        }
+        Assertions.assertFalse(isLost);
     }
 }
