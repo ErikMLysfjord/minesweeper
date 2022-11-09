1b70d8aedd20c855724be807a4ee3506bef87355
Erik Menkin Lysfjord <erikmly@stud.ntnu.no>
2022-11-09 15:14:33 +0100

    feat: add empty restcontroller

---
+++ minesweeper/server/src/main/java/minesweeper/server/HighscoreController.java
@@ -0,0 +1,10 @@
+package minesweeper.server;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("minesweeper")
+public class HighscoreController {
+
+}
