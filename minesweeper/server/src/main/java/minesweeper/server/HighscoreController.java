84b8afb2c090a986ec82227bbc557d62dde5e718
oarorvik <oarorvik@stud.ntnu.no>
2022-11-10 14:55:01 +0100

    feat: add server side POST mapping
    Not tested.

--- minesweeper/server/src/main/java/minesweeper/server/HighscoreController.java
+++ minesweeper/server/src/main/java/minesweeper/server/HighscoreController.java
@@ -2,9 +2,12 @@ package minesweeper.server;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import minesweeper.core.HighscoreEntry;
 import minesweeper.core.HighscoreList;
 
 @RestController
@@ -15,11 +18,21 @@ public class HighscoreController {
     private HighscoreService service;
 
     /**
-     * Get the highscorelist.
-     * @return the highscorelist
+     * Get the highscore list.
+     * @return the highscore list
      */
     @GetMapping("/highscorelist")
     public HighscoreList getHighscoreList() {
         return service.getHighscoreList();
     }
+
+    /**
+     * Saves highscore entry in the server's highscore list.
+     * @param entry to be saved
+     */
+    @PostMapping("/highscorelist")
+    public void addHighscoreEntry(final @RequestBody HighscoreEntry entry) {
+        service.addHighscoreEntry(entry);
+    }
+
 }
