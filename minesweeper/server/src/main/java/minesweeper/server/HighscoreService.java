0187f0292ee26717e06b9ce8d0d6dd2fd164a6b4
Erik Menkin Lysfjord <erikmly@stud.ntnu.no>
2022-11-11 15:05:05 +0100

    feat: make REST API GET and POST with paramater
    The existing GET- and POST-methods have now been extended such that it
    takes in a difficulty-parameter.

--- minesweeper/server/src/main/java/minesweeper/server/HighscoreService.java
+++ minesweeper/server/src/main/java/minesweeper/server/HighscoreService.java
@@ -13,18 +13,22 @@ public class HighscoreService {
 
     /**
      * Gets the highscore read from the json-file.
+     * @param difficulty difficulty chosen
      * @return the highscorelist
      */
-    public HighscoreList getHighscoreList() {
-        return fileHandler.readHighscoreList();
+    public HighscoreList getHighscoreList(final String difficulty) {
+        return fileHandler.readHighscoreList(difficulty);
     }
 
     /**
      * Adds entry to the highscore list in file.
      * @param entry to be added
+     * @param difficulty difficulty chosen
      */
-    public void addHighscoreEntry(final HighscoreEntry entry) {
-        fileHandler.saveScore(entry);
+    public void addHighscoreEntry(final HighscoreEntry entry,
+            final String difficulty) {
+        fileHandler.saveScore(entry, difficulty);
     }
 
 }
+
