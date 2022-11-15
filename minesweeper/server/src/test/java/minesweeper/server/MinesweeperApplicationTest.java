dc6fe37aa19ea0e950d323326565a1c320b77bd9
Erik Menkin Lysfjord <erikmly@stud.ntnu.no>
2022-11-15 22:13:14 +0100

    test: make test use relevant requests
    Instead of MinesweeperApplicationTest sending requests with a custom
    "test"-parameter, it now sends requests with a real difficulty.

--- minesweeper/server/src/test/java/minesweeper/server/MinesweeperApplicationTest.java
+++ minesweeper/server/src/test/java/minesweeper/server/MinesweeperApplicationTest.java
@@ -1,10 +1,10 @@
 package minesweeper.server;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.fail;
 
 import java.io.File;
+import java.io.IOException;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -19,6 +19,8 @@ import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import com.fasterxml.jackson.core.JsonGenerationException;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import minesweeper.core.HighscoreEntry;
@@ -38,16 +40,40 @@ public class MinesweeperApplicationTest {
     private MockMvc mockMvc;
     
     private ObjectMapper mapper;
+    private HighscoreList tempList;
+    private File easyFile = new File(
+        "../core/src/main/resources/minesweeper/json/easyHighscoreList.json"
+    );
 
     @BeforeEach
-    public void setUp() {
+    public void setUp() throws Exception {
         mapper = new HighscoresFileHandler().getMapper();
+        tempList = mapper.readValue(easyFile, HighscoreList.class);
+    }
+
+    private void setNonEmptyList() throws 
+        JsonGenerationException,
+        JsonMappingException,
+        IOException {
+        HighscoreList nonEmptyList = new HighscoreList();
+        HighscoreEntry entry = new HighscoreEntry("Test", 200);
+        nonEmptyList.addEntry(entry);
+        mapper.writeValue(easyFile, nonEmptyList);
+    }
+
+    private void setEmptyList() throws 
+        JsonGenerationException,
+        JsonMappingException,
+        IOException {
+        HighscoreList emptyList = new HighscoreList();
+        mapper.writeValue(easyFile, emptyList);
     }
 
     @Test
     public void testGet() throws Exception {
+        setNonEmptyList();
         MvcResult result = mockMvc.perform(MockMvcRequestBuilders.get(
-            "/minesweeper/highscorelist/Test"
+            "/minesweeper/highscorelist/Easy"
             ).accept(MediaType.APPLICATION_JSON))
             .andExpect(MockMvcResultMatchers.status().isOk())
             .andReturn();
@@ -58,9 +84,8 @@ public class MinesweeperApplicationTest {
                 HighscoreList.class
             );
             assertEquals(5, list.getMaxSize());
-            assertThrows(IndexOutOfBoundsException.class, () -> {
-                list.getHighscoreEntry(0);
-            });
+            assertEquals("Test", list.getHighscoreEntry(0).getName());
+            assertEquals(200, list.getHighscoreEntry(0).getScore());
         } catch (Exception e) {
             fail(e.getMessage());
         }
@@ -68,8 +93,9 @@ public class MinesweeperApplicationTest {
 
     @Test
     public void testPost() throws Exception {
+        setEmptyList();
         HighscoreEntry entry = new HighscoreEntry("Name", 100);
-        String url = "/minesweeper/highscorelist/Test/save";
+        String url = "/minesweeper/highscorelist/Easy/save";
         mockMvc.perform(
             MockMvcRequestBuilders.post(url).contentType(
                 MediaType.APPLICATION_JSON
@@ -78,7 +104,7 @@ public class MinesweeperApplicationTest {
         );
         // Get-method to check if the correct data was sent
         MvcResult result = mockMvc.perform(MockMvcRequestBuilders.get(
-            "/minesweeper/highscorelist/Test"
+            "/minesweeper/highscorelist/Easy"
         ).accept(MediaType.APPLICATION_JSON))
         .andExpect(MockMvcResultMatchers.status().isOk())
         .andReturn();
@@ -96,9 +122,7 @@ public class MinesweeperApplicationTest {
     }
 
     @AfterEach
-    public void tearDown() {
-        new File(
-            "../core/src/main/resources/minesweeper/json/testPersistence.json"
-        ).delete();
+    public void tearDown() throws IOException {
+        mapper.writeValue(easyFile, tempList);
     }
 }
