fc7359fe11fb067946e17adbe4317c80827ff40b
Erik Menkin Lysfjord <erikmly@stud.ntnu.no>
2022-11-17 18:10:18 +0100

    test: write GET- and POST-tests

--- minesweeper/ui/src/test/java/minesweeper/ui/HighscoreAccessTest.java
+++ minesweeper/ui/src/test/java/minesweeper/ui/HighscoreAccessTest.java
@@ -4,7 +4,18 @@ import com.github.tomakehurst.wiremock.WireMockServer;
 import com.github.tomakehurst.wiremock.client.WireMock;
 import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
 
+import minesweeper.core.HighscoreEntry;
+import minesweeper.core.HighscoreList;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.equalTo;
+import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static com.github.tomakehurst.wiremock.client.WireMock.get;
+import static com.github.tomakehurst.wiremock.client.WireMock.equalToJson;
+import static com.github.tomakehurst.wiremock.client.WireMock.verify;
+import static com.github.tomakehurst.wiremock.client.WireMock.postRequestedFor;
 
 import java.net.URI;
 
@@ -26,13 +37,39 @@ public class HighscoreAccessTest {
         wireMockServer.start();
         WireMock.configureFor("localhost", config.portNumber());
         access = new HighscoresAccess(URI.create(
-            "http://localhost:8089/minesweeper")
+            "http://localhost:8080/minesweeper/")
         );
     }
 
     @Test
-    public void test() {
-        assertEquals(1, 1);
+    public void testGetHighscoreList() {
+        stubFor(get(urlEqualTo("/minesweeper/highscorelist/Medium"))
+            .withHeader("accept", equalTo("application/json"))
+            .willReturn(aResponse()
+            .withStatus(200)
+            .withHeader("Content-Type", "application/json")
+            .withBody(
+                "{\"maxSize\":5,\"entries\":[ { \"name\":"
+                + "\"Test\", \"score\": 100 } ]}"
+        )));
+
+        HighscoreList list = access.getHighscoreList("Medium");
+        assertEquals(5, list.getMaxSize());
+        assertEquals("Test", list.getHighscoreEntry(0).getName());
+        assertEquals(100, list.getHighscoreEntry(0).getScore());
+    }
+
+    @Test
+    public void testSaveScore() {
+        HighscoreEntry entry = new HighscoreEntry("Test", 100);
+        access.saveScore(entry, "Medium");
+        // Verifies that the correct POST-request was sent.
+        verify(postRequestedFor(
+            urlEqualTo("/minesweeper/highscorelist/Medium/save"))
+            .withHeader("Content-Type", equalTo("application/json"))
+            .withRequestBody(equalToJson(
+                "{ \"name\":\"Test\", \"score\": 100 }"
+        )));
     }
 
     @AfterEach
